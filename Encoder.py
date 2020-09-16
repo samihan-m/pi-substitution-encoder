ac1ff70cf4db14fdd4f810f8d443db5ce989c6bd
PikaGoku <23440673+PikaGoku@users.noreply.github.com>
2020-09-15 22:29:17 -0700

    COMPLETELY FUNCTIONAL!
    Encode/Decode functions working perfectly!
    
    Might add additional customizability later.

--- Encoder.py
+++ Encoder.py
@@ -3,6 +3,7 @@ Created on Sep 11, 2020
 
 @author: ssmup
 '''
+from _codecs import ascii_encode
 
 class Encoder(object):
     '''
@@ -28,38 +29,166 @@ class Encoder(object):
         
         '''
     
-    def codonChecker(self, piString):
+    def codonChecker(self, piString, codonLength = 3, maxInt = 1000):
         '''
         Takes a length of pi and checks if all possible codons are found within it.
-        A codon is a 3-digit long integer, with values ranging from 000-999.
+        A codon is a 3-digit long integer (by default), with values ranging from 000-maxInt.
+        Can have more or less than 3-digits per codon. NOT WORTH TRYING WITH LENGTH = 4!!!!!! THE REQUIRED SLICE SIZE IS WAY TOO BIG
         Returns true if all codons are found, false if not.
         '''
         codonList = []
-        for i in range(1000):
-            codonStr = '{:0>3}'.format(i)
+        for i in range(maxInt):
+            codonStr = str(i).zfill(codonLength)
+            #codonStr = '{:0<3}'.format(i) poor formatting strategy
             codonList.append(codonStr)
         success = True
-        for i in range(0,1000):
+        for i in range(0,maxInt):
             index = piString.find(codonList[i])
             if(index == -1):
                 success = False
         return success
     
-    def expansiveCodonChecker(self, piString, frontLimit=0):
-        '''
+    """
+    OBSOLETE CODE
+    def expansiveCodonChecker(self, piString, frontLimit=0, codonLength=3, maxInt = 1000):
+        #'''
         Takes a length of pi and checks if all possible codons are found within it. (via codonChecker)
         Trims the beginning of the string if necessary (trimming earlier slices so it doesn't find the same slice)
         If it passes, it removes a character from the end of the pi string and checks it again (also via codonChecker)
         Continues to do so until it fails, then returns the size of the pi string after all of those characters were removed.
         Useful for identifying slices of pi that contain all the codons and where those slices end.
-        '''
+        #'''
         size = len(piString)
         piString = piString[frontLimit:]
-        while(self.codonChecker(piString)):
+        while(self.codonChecker(piString,codonLength,maxInt)):
             piString = piString[:-1]
             size -= 1
         #print("Failed at: size = {0}, frontLimit = {1}".format(size, frontLimit)) #only useful when developing the method/ debugging pi slicing methods
         return size
+    """
+        
+    def piSliceDefiner(self, piString, codonLength = 3, maxInt = 128, detailedConsoleOut = False):
+        '''
+        Takes a length of pi and parses it by adding one character at a time to a new string.
+        When the new string contains every single codon (which range from 0 to maxInt, formatted to have a length of codonLength),
+            it saves the start and end indices of the length of pi copied to the new string into an array.
+        The new string is then reset, and the process repeats from that point in the piString.
+        Returns the array of indices. Format: [[start,end],[start,end],...,[start,end]]
+        WARNING: Very last slice will probably not be accurate if the end of the last entry is the same as the length of the entered parameter piString.
+        Useful for determining slices of pi that contain all the codons and where those slices end for a pre-determined length of pi.
+        '''
+        piSlice = ""
+        meltingPiString = piString
+        floor = 0
+        cap = 0
+        sliceDefinitions = []
+        print("Searching through {0} characters of pi for codons ranging from 0 to {1}...".format(len(piString),maxInt))
+        while(len(meltingPiString) > 0):
+            floorIndex = floor
+            while(self.codonChecker(piSlice,codonLength,maxInt) == False):
+                if(len(meltingPiString) > 0):
+                    piSlice += meltingPiString[0] #adds the first character from the meltingPiString to piSlice
+                    floor += 1
+                    cap += 1
+                    meltingPiString = meltingPiString[1:] #removes the character just added to piSlice from the meltingPiString so as to not add the same character twice
+                else:
+                    return sliceDefinitions #this will fix the issue with the last slice being inaccurate
+                    break
+            capIndex = cap
+            piSlice = ""
+            sliceDefinition = [floorIndex,capIndex]
+            sliceDefinitions.append(sliceDefinition)
+            if(detailedConsoleOut):
+                print("Found {0} breakpoint(s)".format(len(sliceDefinitions)))
+        return sliceDefinitions
+        
+    def numStringSlicer(self, numberString, sliceSize = 3):
+        '''
+        Takes a string of numbers and slices it. Can take a raw int number as well.
+        Returns an array of X-digit strings and some leftover at the end if the string has extra digits.
+        '''
+        numberString = str(numberString)
+        length = len(numberString)
+        sliceCount = length//sliceSize  #this is floor(length/sliceSize)
+        extraDigits = length%sliceSize
+        cleanString = numberString
+        if(extraDigits > 0):
+            cleanString = numberString[:-extraDigits]
+            extraString = numberString[length-extraDigits:]
+        slices = []
+        for i in range(0, sliceCount):
+            index = i*3
+            stringSlice = cleanString[index:index+3]
+            slices.append(stringSlice)
+        if(extraDigits > 0):
+            slices.append(extraString)
+        return slices
+    
+    def numStringSliceFormatter(self, strings, sliceSize = 3):
+        '''
+        Takes an array of numString slices (see numStringSlicer) and formats them to all be the same number of digits.
+        '''
+        formattedStrings = []
+        for element in strings:
+            formatted = element.zfill(sliceSize)
+            formattedStrings.append(formatted)
+        return formattedStrings
+    
+    def codonSubstituter(self, formattedSlicedNumStrings, piSlice):
+        '''
+        Takes an array of formatted numString slices (see numStringSliceFormatter) and returns an array of codon indices that report where the numString slice is found within the piSlice.
+        '''
+        subbedList = []
+        for numStringSlice in formattedSlicedNumStrings:
+            index = piSlice.find(numStringSlice)
+            subbedList.append(index)
+        return subbedList
+    
+    def codonTranslator(self,codonSubbedList, piSlice, codonLength = 3):
+        '''
+        Takes an array of codon indices (see codonSubstituter) and returns an array of formatted numString slices based on the codons found at those indices within the piSlice.
+        '''
+        numStrings = []
+        for codon in codonSubbedList:
+            numString = piSlice[codon:codon+codonLength]
+            numStrings.append(numString)
+        return numStrings
+    
+    def numStringGluer(self,strings, strip = False):
+        '''
+        Takes an array of numString slices and trims leading zeros added by numStringSliceFormatter, glues them together, and returns a single string.
+        Optionally strips extra zeros from the left of each codon, but this is off by default.
+        '''
+        wholeString = ""
+        stringCount = len(strings)
+        i = 0
+        for element in strings:
+            if(strip):
+                if(i == stringCount-1):
+                    wholeString = wholeString + element.lstrip("0")
+                else:
+                    wholeString = wholeString + element
+            else:
+                wholeString = wholeString + element
+            i += 1
+        return wholeString
+    
+    def stringToAscii(self, string):
+        '''
+        Takes a string and returns a string of ascii values, where each character in the original string is turned into an ascii value and concatenated onto the returned string.
+        '''
+        asciiString = ""
+        for character in string:
+            asciiValue = str(ord(character))
+            asciiString += asciiValue.zfill(3)
+        return asciiString
+    
+    def asciiToString(self, asciiSlices):
+        '''
+        Takes an array of ascii codons and turns them into characters before concatenating them. Returns the concatenated string.
+        '''
+        outString = ""
+        for codon in asciiSlices:
+            outString += chr(int(codon))
+        return outString
             
-        
-        
--- Runner.py
+++ Runner.py
@@ -4,21 +4,20 @@ Created on Sep 11, 2020
 @author: ssmup
 '''
 
-#taken from https://www.craig-wood.com/nick/pub/pymath/pi_chudnovsky_bs_gmpy.py
-#requires installation of https://pypi.python.org/pypi/gmpy2
 import math
 from gmpy2 import mpz
 import gmpy2
 from time import time
 from Encoder import Encoder
 
-fridge = []
-
 def pi_chudnovsky_bs(digits):
     """
     Compute int(pi * 10**digits)
 
     This is done using Chudnovsky's series with binary splitting
+    
+    taken from https://www.craig-wood.com/nick/pub/pymath/pi_chudnovsky_bs_gmpy.py
+    requires installation of https://pypi.python.org/pypi/gmpy2
     """
     C = 640320
     C3_OVER_24 = C**3 // 24
@@ -74,64 +73,67 @@ check_digits = {
     1000000 : 58151,
    10000000 : 55897,
 }
-   
-def piSlicer(sliceCount = 10):
+
+"""
+OBSOLETE CODE
+PLEASE IGNORE :)   
+def piSlicer(sliceCount = 10, sliceSize = 14000, codonLength = 3, maxInt = 1000):
     '''
     prints values of Pi at which any less digits would not contain every single possible codon. (codon = 000, 001, .. 999)
-    Then removes that section of the piString and searches again, from the index of the initial breakpoint + 9000 (slightly larger than the index of the first breakpoint, so I'm assuming that all codons can fit in a 9000 digit space)t.
+    Then removes that section of the piString and searches again, from the index of the initial breakpoint + sliceSize (a value slightly larger than the index of the first breakpoint, so I'm assuming that all codons can fit in a 13000 digit space).
     So because 8555 is the first breakpoint, having 8554 digits would not contain every codon, but 8555 digits would contain every codon.
     That means pi_chudnovsky_bs(8555) would have every codon within the string, but pi_chudnovsky_bs(8554) would not.
     So basically it returns acceptable lengths for inputs into pi_chudnovsky_bs.
+    ***ACtually not true. This fails around breakpoint 13 but it passes if I use the breakpoint+1.
+    Can change default codon length from 3 to something else.
     Useful for determining slices of Pi which the encoder can pick to use as a cipher.
-    Not really designed for efficiency so it takes increasingly longer intervals of time to find breakpoints.
-    Gets painfully long at around sliceCount >= 12.
+    Also prints the length of time taken to find the breakpoint.
     '''
-    if(sliceCount <= 0):
+    if(sliceCount < 0):
         sliceCount = 10
-        print("Can't have non-positive sliceCount, defaulting to 10")
-    print("Slicing pi {0} times...".format(sliceCount))
+        print("Can't have negative sliceCount, defaulting to 10")
+    print("Slicing pi {0} times, with an initial slice size of {1}".format(sliceCount, sliceSize))
     
     encoder = Encoder()
-    pi = pi_chudnovsky_bs(10000) #a low number greater than the first break point (which is 8555)
+    pi = pi_chudnovsky_bs(sliceSize) #a low number greater than the first break point (which is 8555 for codonLength = 3)
     piString = str(pi)
-    cap = encoder.expansiveCodonChecker(piString) #this becomes 8555
+    cap = encoder.expansiveCodonChecker(piString, 0, codonLength, maxInt) #this becomes 8555
     print("Breakpoint 0 found: {0}".format(cap))
-    failurePoints = [cap]
+    failurePoints = [0,cap]
+    start = time()
+    lastMoment = 0
     for i in range(0, sliceCount-1):
-        digits = cap+9000
+        digits = cap+sliceSize
         pi = pi_chudnovsky_bs(digits)
         piString = str(pi)
-        cap = encoder.expansiveCodonChecker(piString,cap)
+        cap = encoder.expansiveCodonChecker(piString, cap, codonLength)
         failurePoints.append(cap)
-        print("Breakpoint {0} found: {1}".format(i+1, cap))
+        timeTaken = time()-start
+        print("Breakpoint {0} found: {1}    Time: {2}    TDelta: {3}".format(i+1, cap, '{:5.5}'.format(timeTaken), '{:5.5}'.format(timeTaken-lastMoment)))
+        lastMoment = time()-start
     print("Done slicing!")
     return failurePoints
-
-def getPi(length, startLength = 0):
-    '''
-    A way for me to call pi_chudnovsky_bs that allows me to easily call it and request for substrings beyond a certain initial length.
-    Useful for slicing pi based on breakpoints determined by piSlicer.
-    getPi(breakpoint2, breakpoint1) where breakpoint2 > breakpoint 1 and they're both exact values determined from piSlicer
-    '''
-    pi = pi_chudnovsky_bs(length)
-    piString = str(pi)
-    piString = piString[startLength:]
-    return piString
-
-def storeSlices(sliceCount = 10):
-    '''
-    saves the array holding sliceCount slices to a static variable for future reference by getSliceRange.
-    Defaults to 10, just like piSlicer does. Anything above 10 takes quite a while.
-    This might be useless because I don't know how to make the method variable fridge save to the "class" variable fridge.
-    '''
-    fridge = piSlicer(sliceCount)
     
-def sliceChecker(sliceCount = 10):
+def getPiSlice(sliceNumber):
+    '''
+    Takes in a slice number and returns a size-2 array of the cap and floor indices of the slice.
+    Slice number has to be greater than or equal to 0.
+    '''
+    if(sliceNumber < 0):
+        sliceNumber = 0
+        print("sliceNumber defaulted to 0")
+    allIndices = piSlicer(sliceCount = sliceNumber+1)
+    sliceIndices = []
+    sliceIndices.append(allIndices[sliceNumber+1])
+    sliceIndices.append(allIndices[sliceNumber])
+    return sliceIndices
+    
+def sliceChecker(sliceCount = 10, sliceSize = 14000):
     '''
     Runs piSlicer to get some slices then double checks the validity of each slice.
     Because I'm insecure.
     '''
-    breakpoints = piSlicer(sliceCount)
+    breakpoints = piSlicer(sliceCount, sliceSize)
     encoder = Encoder()
     floor = 0
     for i in range(0,len(breakpoints)):
@@ -142,11 +144,235 @@ def sliceChecker(sliceCount = 10):
             print("FAIL! Check failed between breakpoints {0} ({1}) and {2} ({3})".format(i, breakpoints[i], i-1, floor))
         floor = breakpoints[i]
         
+def sliceAverager(sliceCount = 10):
+    '''
+    Calculates a number of slices then checks the average number of digits in each breakpoint.
+    Useful for statistical purposes?
+    '''
+    fridge = piSlicer(sliceCount)
+    total = 0
+    floor = 0
+    for piSlice in fridge:
+        cap = int(piSlice)
+        total += cap - floor
+        floor = cap
+    print("Average pi slice size: {0}".format(total/len(fridge)))
+    
+def encoderCheck(printStrings=False):
+    '''
+    Uses a bunch of functions from Encoder and an RNG generator to fully test translation of an int value into and out of pi codons.
+    Doesn't always work. See comment at end of function. 80-90% success rate.
+    '''
+    from random import randint
+    encoder = Encoder()
+    randstring = str(randint(10000000,100000000000000))
+    strings = encoder.numStringSlicer(randstring)
+    formattedstrings = encoder.numStringSliceFormatter(strings)
+    codonSubbed = encoder.codonSubstituter(formattedstrings, getPi())
+    resubbed = encoder.codonTranslator(codonSubbed, getPi())
+    reconstituted = encoder.numStringGluer(resubbed)
+    if(printStrings):
+        print(randstring)
+        print(reconstituted)
+    success = False
+    if(randstring == reconstituted):
+        if(printStrings):
+            print("YAY! THIS MEANS TRANSLATION INTO AND OUT OF PI IS SUCCESSFUL!\n")
+        success = True
+    return success
+    '''
+    this is broken becomes sometimes it trims zeros that shouldnt be trimmed.
+    can fix this by only trimming zeros on the last element in reconstituted.
+    I fixed this ^ but now,
+    this also is broken in that if the last term is "0" or "00" or something like that it just gets erased.
+    a potential fix is adding zeros to the end of the initial numstring once it's detected to not be evenly divisible by 3.
+    this ^ is not a potential fix because that will just shift the zero problem; how will it be clear that 0s should be trimmed off the end when retranslating it into the numString?
+    
+    potentially, i don't need to fix anything. because ascii goes from 0 - 127. if ascii 000 = ascii 0, then i can add zeros to the front of the extra codon and not worry about trimming
+    '''
+
+def encoderCheck2(stringLength = 100, printStrings = False, inputString = ""):
+    '''
+    Uses a bunch of functions from Encoder and an RNG generator to fully test translation of a character string into and out of pi codons.
+    This works by taking the character string, turning it into an int string of ascii values, then doing the same process as encoderCheck for number string encoding.
+    Always works. 100% of the time.
+    '''
+    import string
+    import random
+    encoder = Encoder()
+    letters = string.ascii_letters
+    if(inputString != ""):
+        randstring= ''.join(random.choice(letters) for i in range(stringLength))
+    else:
+        randstring = inputString
+    asciiString = encoder.stringToAscii(randstring)
+    strings = encoder.numStringSlicer(asciiString)
+    codonSubbed = encoder.codonSubstituter(strings, getPi())
+    resubbed = encoder.codonTranslator(codonSubbed, getPi())
+    reconstituted = encoder.asciiToString(resubbed)
+    if(printStrings):
+        print(randstring)
+        print(reconstituted)
+    success = False
+    if(randstring == reconstituted):
+        if(printStrings):
+            print("YAY! THIS MEANS TRANSLATION INTO AND OUT OF PI IS SUCCESSFUL!\n")
+        success = True
+    return success
+    
+def measureEncoderSuccess(encoderType, measureCount = 10000, printStrings = False):
+    '''
+    Runs either encoderCheck or encoderCheck2 measureCount times and returns the percentage of successful encoding/decoding.
+    encoderType = 1 --> encoderCheckV2, encoderType = 0 --> faulty encoderCheck
+    '''
+    successCount = 0
+    print('Running encoderCheck {0} times..'.format(measureCount))
+    if(encoderType == 1):
+        print('Using string encoderCheck')
+    else:
+        encoderType = 0
+        print('Using number encoderCheck')
+    for i in range(measureCount):
+        if(encoderType == 0):
+            if(encoderCheck(printStrings)):
+                successCount += 1
+        else:
+            if(encoderCheck2(printStrings)):
+                successCount += 1
+        if(i % 500 == 0):
+            print('Ran {0} times..'.format(i))
+    print('Calculating...')
+    
+    print('Success Percentage: {0}%'.format(successCount*100/measureCount))
+    
+def encode(inputToEncode,piSlice=1):
+    '''
+    Takes an input (int or string, whatever) and encodes it based on the given piSlice.
+    Defaults to the first slice.
+    Returns an array of codons that represent the indices of pi where the input data is found in the piSlice.
+    ''' 
+    sliceBounds = getPiSlice(piSlice)
+    piString = getPi(sliceBounds[0], sliceBounds[1])
+    encoder = Encoder()
+    stringToEncode = str(inputToEncode)
+    asciiString = encoder.stringToAscii(stringToEncode)
+    asciiSlices = encoder.numStringSlicer(asciiString)
+    codonSubbedSlices = encoder.codonSubstituter(asciiSlices, piString)
+    return codonSubbedSlices
+
+def decode(inputToDecode,piSlice=1):
+    '''
+    Takes an input (array of codons) and decodes it based on the given piSlice.
+    Defaults to the first slice.
+    Returns a string of concatenated characters based on the ascii code that the codons represented..
+    '''
+    sliceBounds = getPiSlice(piSlice)
+    piString = getPi(sliceBounds[0], sliceBounds[1])
+    encoder = Encoder()
+    asciiSubbedSlices = encoder.codonTranslator(inputToDecode, piString)
+    reconstitutedString = encoder.asciiToString(asciiSubbedSlices)
+    return reconstitutedString
+"""
+def getPi(length = 8555, startLength = 0):
+    '''
+    A way for me to call pi_chudnovsky_bs that allows me to easily call it and request for substrings beyond a certain initial length.
+    Useful for slicing pi based on breakpoints determined by piSlicer.
+    getPi(breakpoint2, breakpoint1) where breakpoint2 > breakpoint 1 and they're both exact values determined from piSlicer
+    Defaults to 8555 because that's the first breakpoint with codon length = 3 with codons going from 0 - 999.
+    Actually returns a length of 2 more than the length parameter. Intentionally.
+    '''
+    pi = pi_chudnovsky_bs(length+1) #this is length+1 because breakpoint 13 fails without it. I have a hypothesis about this where the breakpoint works 99% of the time because of RNG but it's not really supposed to without the +1
+    piString = str(pi)
+    piString = piString[startLength:]
+    return piString
+
+def encodeByBounds(inputToEncode,sliceBounds=[0, 3853]):
+    '''
+    Takes an input (int or string, whatever) and encodes it based on the given sliceBounds.
+    Defaults to the first slice for codons of length 3 ranging from 0 to 128.
+    Assumes the slice contains all codons required to encode the input.
+    Returns an array of codons that represent the indices of pi where the input data is found in the piSlice.
+    ''' 
+    piString = getPi(sliceBounds[1], sliceBounds[0])
+    encoder = Encoder()
+    stringToEncode = str(inputToEncode)
+    asciiString = encoder.stringToAscii(stringToEncode)
+    asciiSlices = encoder.numStringSlicer(asciiString)
+    codonSubbedSlices = encoder.codonSubstituter(asciiSlices, piString)
+    return codonSubbedSlices
+
+def decodeByBounds(inputToDecode,sliceBounds=[0, 3853]):
+    '''
+    Takes an input (array of codons) and decodes it based on the given sliceBounds.
+    Defaults to the first slice for codons of length 3 ranging from 0 to 128.
+    Assumes the slice contains all codons required to decode the input.
+    Returns a string of concatenated characters based on the ascii code that the codons represented..
+    '''
+    piString = getPi(sliceBounds[1], sliceBounds[0])
+    encoder = Encoder()
+    asciiSubbedSlices = encoder.codonTranslator(inputToDecode, piString)
+    reconstitutedString = encoder.asciiToString(asciiSubbedSlices)
+    return reconstitutedString
+    
 if __name__ == "__main__":
-    sliceChecker(100)
+    string = "The color of animals is by no means a matter of chance; it depends on many considerations, but in the majority of cases tends to protect the animal from danger by rendering it less conspicuous. Perhaps it may be said that if coloring is mainly protective, there ought to be but few brightly colored animals. There are, however, not a few cases in which vivid colors are themselves protective. The kingfisher itself, though so brightly colored, is by no means easy to see. The blue harmonizes with the water, and the bird as it darts along the stream looks almost like a flash of sunlight."
+    string2 = "He killed 100239 people."
+    string = '"Are you insane?", he said.'
+    #print(encoderCheck2(inputString = string))
+    #print(encoderCheck2(inputString = string2))
+    
+    #piSlicer(sliceCount=5,maxInt=128)
+    piString = getPi(10000)
+    encoder = Encoder()
+    slices = encoder.piSliceDefiner(piString)
+    for indices in slices:
+        print(indices)
+    #sliceBounds = slices[3]
+    
+    '''
+    encoded = encodeByBounds(string)
+    print(string)
+    decoded = decodeByBounds(encoded)
+    print(decoded)
+    
+    print("test 2")
+    
+    encoded = encodeByBounds(string,sliceBounds)
+    print(string)
+    decoded = decodeByBounds(encoded,sliceBounds)
+    print(decoded)
+    '''
+    
+    '''
+    maybe add an array of pre-generated pi values to use... waiting 20 seconds every time you encode something is a pain in the ass
+    BUT EITHER WAY
+    THE ENCODING IS WORKING PERFECTLY!
+    IT"S AMAZING!
+    to-do:
+    add a function that works by generating increasing lengths of pi (digit by digit) and notes the length it takes to have all codons.
+    kind of like expansiveCodonChecker but instead of slowly trimming pi it slowly builds on pi.
+    this way it can just keep generating more and more and more and get precise data about breakpoints instead of failing sometimes because the gap is larger than 14k or whatever i set it to through piSlicer
+    
+    once this ^ function is made i can clear up some old functions
+    '''
+    #measureEncoderSuccess(100000000)
+    #encoderCheck2(printStrings = True)
+    #measureEncoderSuccess(1)
+    #sliceChecker(500, 12000)
+    #this fails at breakpoints 145, 372, 432, 449.
+    #piSlicer(sliceCount=10,codonLength=3)
+    #sliceChecker(10)
+    
+    #print(encoder.codonChecker(getPi(106872,100771)))
+    #print(encoder.codonChecker(getPi(24775,16170)))
     '''
     TODO LIST:
-    Maybe calculate the average size of each slice of pi? That could be interesting.
+    Maybe calculate the average size of each slice of pi? That could be interesting. done
+    *****KEY: (not really important. i can develop the rest of the functionality for just the first slice of pi)
+        Create a function to automatically re-check breakpoints as it finds them - 
+        so it dynamically notices that after adding 10000 (or whatever) the breakpoint failed, and then have it re-add 10000 (or whatever)
+            and have it wait until proper failure
+            (where proper failure means expansiveCodonChecker goes through more than just 1 character down from the piString).
     Add the translation feature. So far this is just a PI Scanner
     Maybe figure out how to make the codonChecker a binary search or a tree search instead of a sequential search.
     Doesn't really make sense because the numbers need to be in order, but if there's a better way I should take it.
